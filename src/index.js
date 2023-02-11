6589a84b771c54240158747cf89d7878d61c7d15
Gabriel Rufino <contato@gabrielrufino.com>
2023-02-11 15:04:27 -0300

    Functional version

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import { Bot } from 'grammy'
+import fs from 'node:fs/promises'
+import os from 'node:os'
+import path from 'node:path'
+
+import { Bot, InputFile } from 'grammy'
+import puppeteer from 'puppeteer'
+
+import { logger } from './logger.js'
 
 const {
-    BOT_TOKEN
+  BOT_TOKEN
 } = process.env
 
 const bot = new Bot(BOT_TOKEN)
 
 bot.command('start', context => context.reply('Welcome to PDFfromLink! Send a link and receive the PDF.'))
 bot.on('message', async context => {
-    context.reply('Soon')
+  const { text } = context.message
+
+  const browser = await puppeteer.launch()
+
+  const page = await browser.newPage()
+  await page.goto(text)
+
+  const folder = await fs.mkdtemp(path.join(os.tmpdir(), 'pdffromlink-'))
+  const filePath = path.join(folder, 'file.pdf')
+  await page.pdf({ path: filePath })
+
+  const title = await page.title()
+
+  await Promise.all([
+    browser.close(),
+    context.replyWithDocument(new InputFile(filePath, `${title}.pdf`))
+  ])
+
+  logger.info({ context })
 })
 
-bot.start().then(() => console.log('Bot running'))
+bot.start()
